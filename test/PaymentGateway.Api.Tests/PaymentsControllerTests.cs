2cfda427e0e1bdbceb8c7438cf4ee3dcc0c6ac43
giacholari <giacholari@gmail.com>
2026-03-27 18:36:07 +0000

    add validation test

--- test/PaymentGateway.Api.Tests/PaymentsControllerTests.cs
+++ test/PaymentGateway.Api.Tests/PaymentsControllerTests.cs
@@ -71,6 +71,7 @@ public class PaymentsControllerTests
     [Fact]
     public async Task ProcessesAPaymentSuccessfully()
     {
+        // Arrange
         var mockAcquiringBankService = new Mock<IAcquiringBankService>();
         mockAcquiringBankService
             .Setup(x => x.ProcessPaymentAsync(It.IsAny<AcquiringBankPostPaymentRequest>()))
@@ -84,8 +85,11 @@ public class PaymentsControllerTests
         // authorized response when the last digit falls in 1, 3, 5, 7, 9
         var request = CreateStubPostPaymentRequest("4242424242424241");
         
+        // Act
         var response = await client.PostAsJsonAsync("/api/Payments", request);
         var result = await response.Content.ReadFromJsonAsync<PostPaymentResponse>();
+        
+        // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.NotNull(result);
         Assert.Equal("Authorized", result?.Status);
@@ -94,6 +98,7 @@ public class PaymentsControllerTests
     [Fact]
     public async Task TheAcquiringBankReturnsDeclined()
     {
+        // Arrange
         var mockAcquiringBankService = new Mock<IAcquiringBankService>();
         mockAcquiringBankService
             .Setup(x => x.ProcessPaymentAsync(It.IsAny<AcquiringBankPostPaymentRequest>()))
@@ -108,8 +113,11 @@ public class PaymentsControllerTests
         // unauthorized response when the last digit falls in 2, 4, 6, 8
         var request = CreateStubPostPaymentRequest("4242424242424242");
         
+        // Act
         var response = await client.PostAsJsonAsync("/api/Payments", request);
         var result = await response.Content.ReadFromJsonAsync<PostPaymentResponse>();
+       
+        // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.NotNull(result);
         Assert.Equal("Declined", result?.Status);
@@ -118,6 +126,7 @@ public class PaymentsControllerTests
     [Fact]
     public async Task AcquiringBankIsUnavailable()
     {
+        // Arrange
         var mockAcquiringBankService = new Mock<IAcquiringBankService>();
         mockAcquiringBankService
             .Setup(x => x.ProcessPaymentAsync(It.IsAny<AcquiringBankPostPaymentRequest>()))
@@ -128,8 +137,11 @@ public class PaymentsControllerTests
         // unavailable response when the last digit is 0
         var request = CreateStubPostPaymentRequest("4242424242424240");
         
+        // Act
         var response = await client.PostAsJsonAsync("/api/Payments", request);
         var content = await response.Content.ReadAsStringAsync();
+        
+        // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         Assert.Contains("The service is unavailable right now. No money was taken from your card. Please try again later", content);
     }
@@ -137,6 +149,7 @@ public class PaymentsControllerTests
     [Fact]
     public async Task EnsureValidation()
     {
+        // Arrange
         var factory = new WebApplicationFactory<PaymentsController>();
         var client = factory.CreateClient();
         var request = new PostPaymentRequest
@@ -149,8 +162,10 @@ public class PaymentsControllerTests
             Cvv = "1"
         };
         
+        // Act
         var response = await client.PostAsJsonAsync("/api/Payments", request);
         
+        // Assert
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
---
+++ test/PaymentGateway.Api.Tests/PostPaymentRequestValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using PaymentGateway.Api.Models.Requests;
+using PaymentGateway.Api.Validation.Requests;
+
+namespace PaymentGateway.Api.Tests;
+
+public class PostPaymentRequestValidatorTest
+{
+    private readonly PostPaymentRequestValidator _validator = new();
+    
+    [Theory]
+    [InlineData(12, 1999, "GBP", "Expiry year is earlier than current year")] // very old card
+    [InlineData(1, 2026, "GBP", "The card has expired")]   // expired card
+    [InlineData(1, 2028, "XYZ", "The currency is invalid")]   // invalid currency
+    public async Task ValidatesTheRequest(int expiryMonth, int expiryYear, string currency, string error)
+    {
+        // Arrange
+        var request = new PostPaymentRequest
+        {
+            CardNumber = "4242424242424243",
+            ExpiryMonth = expiryMonth,
+            ExpiryYear = expiryYear,
+            Currency = currency,
+            Amount = 1,
+            Cvv = "123"
+        };
+        
+        // Act
+        var errors = _validator.Validate(request);
+        
+        // Assert
+        Assert.NotEmpty(errors);
+        Assert.Contains(errors, e => e == error);
+    }
+}
